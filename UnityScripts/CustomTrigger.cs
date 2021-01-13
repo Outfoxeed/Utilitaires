29e40d97f97187c757fa826d98ceb5e22d0a8efc
Outfoxeed <letourneaumorigan@gmail.com>
2021-01-13 12:43:48 +0100

    Create CustomTrigger.cs

---
+++ UnityScripts/CustomTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class CustomTrigger : MonoBehaviour
+{
+    public enum OnTriggerAction { destroy, disable, disable_trigger};
+    public OnTriggerAction actionOnTrigger;
+    public UnityEvent evnt;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            evnt?.Invoke();
+            switch (actionOnTrigger)
+            {
+                case OnTriggerAction.destroy:
+                    Destroy(gameObject);
+                    break;
+                case OnTriggerAction.disable:
+                    gameObject.SetActive(false);
+                    break;
+                case OnTriggerAction.disable_trigger:
+                    GetComponent<Collider2D>().enabled = false;
+                    break;
+            }
+        }
+    }
+}
